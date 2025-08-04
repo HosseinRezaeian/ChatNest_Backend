d8100a26331cfe4168e6c7215cae9f0fa510c62b
hosseinrezaeian <hossainrezaeian@gmail.com>
2025-08-04 22:51:09 +0330

    add token get

--- apps/chatWebsocket/api/view.py
+++ apps/chatWebsocket/api/view.py
@@ -1,8 +1,14 @@
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.views import APIView
+from itsdangerous import TimestampSigner, BadSignature, SignatureExpired
 
 
 class SocketToken(APIView):
     permission_classes = (IsAuthenticated,)
     def post(self, request):
-        return ("")
+        signer = TimestampSigner('your-secret-key')
+        token = signer.sign(f'user_id:{request.user.id}').decode()
+        print(token)
+        return Response({'token': token},status=status.HTTP_200_OK)
